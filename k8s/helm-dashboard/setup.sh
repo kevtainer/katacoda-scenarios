4832b856d57bbd53c08b91c939e1c9358e9b1d21
kevin crawley <kevin@singlemusic.co>
2020-05-30 14:17:01 -0500

    refine init

--- k8s/helm-dashboard/setup.sh
+++ k8s/helm-dashboard/setup.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
+launch.sh
+
 mkdir -p /home/traefik; cd /home/traefik
+
+DEBIAN_FRONTEND=noninteractive apt install -qq apache2-utils
+
+{ clear && echo "Traefik School is in session.";} 2> /dev/null
